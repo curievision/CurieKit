fb2c01cdb1ba112f1f31babcf5a3cdd3aed9349b
Benji Dodgson <benjamindodgson@gmail.com>
2024-01-30 16:11:09 -0800

    Adding the view model and window id

---
+++ Sources/CurieKit/Models/CurieProductVolumeIdentifier.swift
@@ -0,0 +1,10 @@
+//
+//  CurieWindowGroup.swift
+//
+//
+//  Created by Benji Dodgson on 1/30/24.
+//
+
+import Foundation
+
+public let CurieProductVolumeIdentifier: String = "CurieProductVolumeIdentifier"
