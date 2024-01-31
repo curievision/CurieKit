e058ffe5ba409d12e90c662a56331fe052cc5d08
Benji Dodgson <benjamindodgson@gmail.com>
2024-01-31 14:54:34 -0800

    Adding ios support

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CurieKit",
-    platforms: [.visionOS(.v1)],
+    platforms: [.visionOS(.v1), .iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
